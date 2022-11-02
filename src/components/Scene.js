f92ab9aaf86929347a63dc188c852ca40b2d0517
Laura Hiller <Hillerelaura@gmail.com>
2022-11-02 15:18:12 -0700

    Added ground object

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,15 +1,19 @@
 import './Scene.css';
 import { Canvas } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
-import { PointerLockControls } from '@react-three/drei'
+//import { PointerLockControls } from '@react-three/drei'   Leave this commented out for now, will use orbit controls for setting up scene
+import { OrbitControls } from "@react-three/drei";
+import Ground from './Ground';
 
 function Scene() {
   return (
     <div id="canvas-wrap">
         <Canvas>
           <color attach="background" args={['#000000']} />
+          {/*<PointerLockControls />*/}
+          <OrbitControls />
           <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
-          <PointerLockControls />
+          <Ground />
         </Canvas>
     </div>
   )
