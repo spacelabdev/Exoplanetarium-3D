1380ece995d4cb99c84a091f1a221e4e6d201f48
jrkozak92 <jrkozak92@gmail.com>
2023-02-19 15:05:11 -0600

    Helper functions implemeneted for converting sexagesimal to decimal, string values on data are properly parsed and populated in canvas.

--- src/components/Scene/conversionsHelper.js
+++ src/components/Scene/conversionsHelper.js
@@ -1,5 +1,13 @@
-const convertRAtoDecimal = (RAString) => {
-    let hours = RAString.matchAll(/(\d\d\W?)/g)
-    let minutes
-    let seconds
+export const convertSexagesimalToDecimal = (SegString) => {
+    const parsedSexagesimalArray = [...SegString.matchAll(/-?\+?(\d\d\W?)/g)]
+    let decimalValue = ((parseInt(parsedSexagesimalArray[0]) * 15) + (parseInt(parsedSexagesimalArray[1]) / 4) + (parseInt(parsedSexagesimalArray[2]) / 240))
+    return decimalValue
+}
+
+export const convertAllSexagesimalToDec = (dataArray) => {
+    dataArray.forEach((planet, i)=> {
+        dataArray[i].rightAscension = convertSexagesimalToDecimal(planet.rightAscension)
+        dataArray[i].declination = convertSexagesimalToDecimal(planet.declination)
+    } )
+    return dataArray
 }
