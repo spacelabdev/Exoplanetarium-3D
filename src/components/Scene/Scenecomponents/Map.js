f7d7833af541c853495086ca2fbd31384e6b1146
Laura Hiller <Hillerelaura@gmail.com>
2023-02-19 12:29:00 -0800

    Sent data on each planet into instanced mesh

--- src/components/Scene/Scenecomponents/Map.js
+++ src/components/Scene/Scenecomponents/Map.js
@@ -1,8 +1,8 @@
-import {React, useEffect, useRef, useState} from 'react';
-import { useFrame } from '@react-three/fiber';
-import { Instances } from '@react-three/drei';
-import * as THREE from 'three';
-import Planet from './Planet';
+import { React, useEffect, useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Instances } from "@react-three/drei";
+import * as THREE from "three";
+import Planet from "./Planet";
 
 /*https://codesandbox.io/s/floating-instanced-shoes-h8o2d*/
 //Converting from celestial coordinates to cartesian coordinates
@@ -12,29 +12,36 @@ import Planet from './Planet';
 const planetObject = new THREE.Object3D();
 
 function Map({ data }) {
-    const numPlanets = data.length;
-    const ref = useRef()
-    useEffect(() => {
-      // Set positions
-      let count = 0;
-      for(let d = 0; d < numPlanets; ++d){
-        ++count;
-        console.log(data[d]);
-        const x = data[d].distance * Math.cos(data[d].declination) * Math.cos(data[d].rightAscension);
-        const y = data[d].distance * Math.cos(data[d].declination) * Math.sin(data[d].rightAscension);
-        const z = data[d].distance * Math.sin(data[d].declination);
-        planetObject.position.set(x,y,z);
-        planetObject.updateMatrix();
-        ref.current.setMatrixAt(count, planetObject.matrix);
-      }
-      // Update the instance
-      ref.current.instanceMatrix.needsUpdate = true
-    }, [])
-    return (
-      <instancedMesh ref={ref} args={[null, null, numPlanets]}>
-        <Planet />
-      </instancedMesh>
-    )
-  }
+  const numPlanets = data.length;
+  const ref = useRef();
+  useEffect(() => {
+    // Set positions
+    let count = 0;
+    for (let d = 0; d < numPlanets; ++d) {
+      ++count;
+      const x =
+        data[d].distance *
+        Math.cos(data[d].declination) *
+        Math.cos(data[d].rightAscension);
+      const y =
+        data[d].distance *
+        Math.cos(data[d].declination) *
+        Math.sin(data[d].rightAscension);
+      const z = data[d].distance * Math.sin(data[d].declination);
+      planetObject.position.set(x, y, z);
+      planetObject.updateMatrix();
+      ref.current.setMatrixAt(count, planetObject.matrix);
+    }
+    // Update the instance
+    ref.current.instanceMatrix.needsUpdate = true;
+  }, []);
+  return (
+    <instancedMesh ref={ref} args={[null, null, numPlanets]}>
+      {data.map((props, i) => (
+        <Planet key={i} {...props} />
+      ))}
+    </instancedMesh>
+  );
+}
 
 export default Map;
--- src/components/Scene/Scenecomponents/Planet.js
+++ src/components/Scene/Scenecomponents/Planet.js
@@ -1,13 +1,14 @@
-import {React, useRef} from 'react';
+import { React, useRef } from "react";
 
 function Planet(props) {
-    const ref = useRef()
-    return (
-        <>
-            <sphereGeometry args={[.1, 10, 10]} {...props} />
-            <meshLambertMaterial color="purple"/>
-        </>
-    )
-  }
+  console.log(props);
+  const ref = useRef();
+  return (
+    <>
+      <sphereGeometry args={[0.1, 10, 10]} {...props} />
+      <meshLambertMaterial color="purple" />
+    </>
+  );
+}
 
 export default Planet;
