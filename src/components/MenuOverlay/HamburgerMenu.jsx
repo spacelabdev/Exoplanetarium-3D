4f85686135761a1362806009e555057d72390d4f
Teju Edidi <tejuedidi@gmail.com>
2023-07-18 15:00:30 -0700

    added open and close functionality, configuring proper menu overlay dimensions

--- src/components/MenuOverlay/HamburgerMenu.jsx
+++ src/components/MenuOverlay/HamburgerMenu.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Divide as Hamburger } from 'hamburger-react'
+import { IoMenuSharp, IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
 import "./HamburgerMenu.scss";
 
 function MenuOverlay() {
@@ -8,18 +10,38 @@ function MenuOverlay() {
 
     return (
         <div className="MenuOverlay">
-            {/* <Hamburger label="Show menu" />  */}
-            <Hamburger onToggle={setIsOpen}/>
-            {isOpen && ( //if menu is open render the menu items
-            <div className="menu-items">
-                <Link to="/">Tutorial</Link>
-                <Link to="/">Settings</Link>
-                <Link to="/">Contact Us</Link>
-                <Link to="/">Menu Item</Link>
+            <div className="menu-button" onClick={() => setIsOpen(!isOpen)}>
+                {/* <Hamburger toggled={isOpen} toggle={setIsOpen} /> */}
+                <IoMenuSharp size={35} />
             </div>
-            )}
+
+            <Menu // slide transition
+                right
+                isOpen={isOpen}
+                onStateChange={({ isOpen }) => setIsOpen(isOpen)}
+                customCrossIcon={false}
+                width={"250px"}
+                itemListElement="div"
+            >
+
+                <div className="menu-box">
+                    <div className="menu-items">
+                        <h2>Menu</h2>
+                        <Link to="/" style={{ textDecoration: "none" }}> Tutorial</Link>
+                        <Link to="/" style={{ textDecoration: "none" }}> Settings</Link>
+                        <Link to="/" style={{ textDecoration: "none" }}> Contact Us</Link>
+                        <Link to="/" style={{ textDecoration: "none" }}> Menu Item</Link>
+                    </div>
+                </div>
+                {isOpen && ( // if menu is open render the menu items
+                    // wrapper for when open
+                    <div className="menu-close-button" onClick={() => setIsOpen(false)}>
+                        <IoCloseSharp size={35} />
+                    </div>
+                )}
+            </Menu>
         </div>
     );
 }
-  
+
 export default MenuOverlay;
