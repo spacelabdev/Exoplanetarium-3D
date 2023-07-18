860f2fbe6ec7a7396d123c0dc1fa3add72d6018d
Jaspreet <jaspreetkn92@gmail.com>
2023-07-18 13:01:48 +0100

    added routes to the main App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router';
 import Visualize from './pages/Visualize';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Welcome from './pages/Welcome';
+import FAQ from './components/FAQ/FAQ';
+import DemoVideo from './components/DemoVideo/DemoVideo';
+import Walkthrough from './components/Walkthrough/Walkthrough';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Visualize />} />
+        <Route exact path="/welcome" element={<Welcome />} />
+        <Route exact path="/FAQ" element={<FAQ />} />
+        <Route exact path="/demovideo" element={<DemoVideo />} />
+        <Route exact path="/walkthrough" element={<Walkthrough />} />
       </Routes>
       <Footer />
     </div>
