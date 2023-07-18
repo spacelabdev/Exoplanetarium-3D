2daa480f3a9e955327cf6c60d099ba06203f10ae
Jaspreet <jaspreetkn92@gmail.com>
2023-07-18 12:48:08 +0100

    added Poppins font in App SCSS aligned with font-family in Figma design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.scss";
-import { Route, Routes } from "react-router";
-import Visualize from "./pages/Visualize";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import './App.scss';
+import { Route, Routes } from 'react-router';
+import Visualize from './pages/Visualize';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Header />
       <Routes>
         <Route path="/" element={<Visualize />} />
