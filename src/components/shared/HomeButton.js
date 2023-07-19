e688786466644670532f1c9e9f820893ab964e37
Jaspreet <jaspreetkn92@gmail.com>
2023-07-19 07:11:24 +0100

    created HomeButton component and routed to the Welcome page

---
+++ src/components/shared/HomeButton.js
@@ -0,0 +1,12 @@
+import { AiOutlineHome } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
+
+function HomeButton() {
+  return (
+    <NavLink to="/welcome" className="App-home-button">
+      <AiOutlineHome size={50} />
+    </NavLink>
+  );
+}
+
+export default HomeButton;
