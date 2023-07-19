b6f6ebc5c0e7a769440e1676fc9fc84669713cd8
Jaspreet <jaspreetkn92@gmail.com>
2023-07-19 11:59:02 +0100

    added FAQ functionality

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,11 +1,68 @@
-import HomeButton from '../shared/HomeButton';
+import HomeButton from '../../components/shared/HomeButton';
+import './FAQ.css';
+import { useState } from 'react';
 
 function FAQ() {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
+  const data = [
+    {
+      question: 'How does it work?',
+    },
+
+    {
+      question: 'How does it work?',
+    },
+
+    {
+      question: 'How does it work?',
+    },
+
+    {
+      question: 'How does it work?',
+    },
+
+    {
+      question: 'How does it work?',
+    },
+  ];
+
   return (
-    <>
-      <h1>FAQ</h1>
+    <section className="faq-container">
+      <div className="faq-content">
+        <h1>Questions? Look Here.</h1>
+        <p>Still can't find an answer? Email us at fakeemail@gmail.com</p>
+        {data.map((item, index) => (
+          <section
+            className="faq-accordion"
+            onClick={() => toggle(index)}
+            key={index}
+          >
+            <div className="aq">
+              <p>{item.question}</p>
+
+              {clicked === index && (
+                <p className="App-faq-answer">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+                  praesentium.
+                </p>
+              )}
+            </div>
+            <span>{clicked === index ? '-' : '+'}</span>
+          </section>
+        ))}
+      </div>
+
       <HomeButton />
-    </>
+    </section>
   );
 }
 
