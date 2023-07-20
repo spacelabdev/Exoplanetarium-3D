a032637f9f27d705bc04b7e2c3a098a45f761d76
Jaspreet <jaspreetkn92@gmail.com>
2023-07-20 18:03:36 +0100

    added the rectangles to the links

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,8 +6,18 @@ function Welcome() {
     <div className="App-welcome">
       <main className="App-container">
         <h1>Welcome to Exoplanetarium by SpaceLab</h1>
-        <Link to="/walkthrough">Walkthrough</Link>
-        <Link to="/demovideo">Demo Video</Link>
+        <div className="rectangle">
+          <div className="left-rectangle"></div>
+          <Link to="/walkthrough">Walkthrough</Link>
+          <div className="right-rectangle"></div>
+        </div>
+
+        <div className="rectangle">
+          <div className="left-rectangle demo"></div>
+          <Link to="/demovideo">Demo Video</Link>
+          <div className="right-rectangle demo"></div>
+        </div>
+
         <Link to="/FAQ">FAQ</Link>
       </main>
     </div>
