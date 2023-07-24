817ac8e721b5b7d84c5209e3a77403108b3603aa
Jaspreet <jaspreetkn92@gmail.com>
2023-07-24 07:15:32 +0100

    added slide 4

---
+++ src/components/Walkthrough/Slide4.js
@@ -0,0 +1,11 @@
+import './Walkthrough.scss';
+
+function Slide4() {
+  return (
+    <section className="slide4">
+      <p>Click on any star or planet to view more information</p>
+    </section>
+  );
+}
+
+export default Slide4;
