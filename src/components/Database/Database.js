831276daa6f8d556602730e042984c29d3af5703
Laurie Liu <lauriealiu@gmail.com>
2023-07-25 21:18:27 -0700

    Basic UI finished

---
+++ src/components/Database/Database.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+// Replace with your actual database icon image
+import databaseIcon from "../../assets//database.png";
+
+const Database = () => {
+  const [showDatabase, setShowDatabase] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Static array
+  const staticExoplanets = [
+    { name: 'Exoplanet 1', distance: '100 light-years' },
+    { name: 'Exoplanet 2', distance: '200 light-years' },
+    { name: 'Exoplanet 3', distance: '150 light-years' },
+    // Add more exoplanets as needed
+  ];
+
+  const handleIconClick = () => {
+    setShowDatabase(true);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredExoplanets = staticExoplanets.filter((planet) =>
+    planet.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <img
+        src={databaseIcon}
+        alt="Database Icon"
+        className="database-icon"
+        onClick={handleIconClick}
+      />
+
+      {showDatabase && (
+        <div className="exoplanet-database">
+          <div className="header">
+            <div className="close-button" onClick={() => setShowDatabase(false)}>
+              X
+            </div>
+            <h2>Exoplanetarium Database</h2>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div className="exoplanets">
+            {filteredExoplanets.map((planet) => (
+              <div className="exoplanet-item" key={planet.name}>
+                <h3>{planet.name}</h3>
+                <p>Distance: {planet.distance}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Database;
