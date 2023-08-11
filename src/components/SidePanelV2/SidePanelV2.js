d6318756d6718ca0244cb5b83aa67f1d09ce7c63
Teju Edidi <tejuedidi@gmail.com>
2023-08-11 13:01:20 -0700

    removed close button and added exoplanet data

--- src/components/SidePanelV2/SidePanelV2.js
+++ src/components/SidePanelV2/SidePanelV2.js
@@ -1,6 +1,5 @@
 import "./SidePanelV2.scss";
 import { React, useState } from "react";
-import { IoCloseSharp } from "react-icons/io5";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md"
 import returnToSpace from '../../assets/emojione-v1_milky-way.svg'
 import planetIcon from '../../assets/HighlightedPlanet.svg'
@@ -26,20 +25,10 @@ function SidePanel({ planet }) {
     planet ? planet : defaultValue;
   const { stellarDistance, effectiveTemperature, log, radius } = starData;
 
-  const [panelStatus, setPanelStatus] = useState(true)
   const [hovering, setHovering] = useState(false)
-
-  // const handleLinkClick = () => {
-  //   setLinkClicked(true)
-  // }
-
-  const handleCloseClick = () => {
-    setPanelStatus(false)
-  }
 
   const handleReturnClick = () => {
     window.location.reload(false)
-    // setPanelStatus(true)
   }
 
   const handleHover = () => {
@@ -92,35 +81,67 @@ function SidePanel({ planet }) {
 
   return (
     <>
-      {/* <p className={`${planet ? "show-link" : "hide-link"} 
-      ${linkClicked ? "hide-link" : "show-link"}`} onClick={handleLinkClick}> Exoplanet {name}</p> */}
-
-      <aside className={planet && panelStatus ? "side-panel open" : "side-panel"}>
-
-        {/* <aside className={planet && linkClicked ? "side-panel open" : "side-panel"}> */}
+      <aside className={planet ? "side-panel open" : "side-panel"}>
         <p className="sys-name">NAME System</p>
 
         <section className="panel-title-container">
           <p className="panel-title"> {name}</p>
 
           <div className="button-wrap">
-            <button className="return-n-close" onClick={handleReturnClick} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
+            <button className="return" onClick={handleReturnClick} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
               <img src={returnToSpace} alt=" returnToSpace" className="return-hover" />
             </button>
-
             {hovering && <p className="hover-text">Return to space </p>}
-
-            <button className="return-n-close" onClick={handleCloseClick}>
-              <IoCloseSharp />
-            </button>
           </div>
         </section>
 
         <section className="panel-content">
-          <p className="content-text">
+          <div className="planetData-wrap">
+            <p>Exoplanet Data</p>
+            <ul>
+              <li>
+                <span className="styled-text">{"Disposition: "}</span>
+                {disposition}
+              </li>
+              <li>
+                <span className="styled-text">{"Right Ascension: "}</span>
+                {rightAscension}
+              </li>
+              <li>
+                <span className="styled-text">{"Declination: "}</span>
+                {declination}
+              </li>
+              <li>
+                <span className="styled-text">{"Distance: "}</span>{" "}
+                {distance + " Parsecs"}
+              </li>
+            </ul>
+          </div>
+
+          <div className="stellarData-wrap">
+            <p>Stellar Data</p>
+            <ul>
+              <li>
+                <span className="styled-text">{"Stellar Distance: "}</span>{" "}
+                {stellarDistance + " Parsecs"}
+              </li>
+              <li>
+                <span className="styled-text">{"Effectve Temperature: "}</span>{" "}
+                {effectiveTemperature + " K"}
+              </li>
+              <li>
+                <span className="styled-text">{"Log: "}</span> {log + " cm/s**2"}
+              </li>
+              <li>
+                <span className="styled-text">{"Radius: "}</span>{" "}
+                {radius + " R_Sun"}
+              </li>
+            </ul>
+          </div>
+          {/* <p className="content-text">
             Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
             Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
-          </p>
+          </p> */}
         </section>
 
         <section className="planet-slider-container">
@@ -147,50 +168,6 @@ function SidePanel({ planet }) {
           </section>
           <p>Exoplanet {name}</p>
         </section>
-
-        {/* <div className={"info-wrap"}>
-        <div className="planetData-wrap">
-          <p>Exoplanet Data</p>
-          <ul>
-            <li>
-              <span className="styled-text">{"Disposition: "}</span>
-              {disposition}
-            </li>
-            <li>
-              <span className="styled-text">{"Right Ascension: "}</span>
-              {rightAscension}
-            </li>
-            <li>
-              <span className="styled-text">{"Declination: "}</span>
-              {declination}
-            </li>
-            <li>
-              <span className="styled-text">{"Distance: "}</span>{" "}
-              {distance + " Parsecs"}
-            </li>
-          </ul>
-        </div>
-        <div className="stellarData-wrap">
-          <p>Stellar Data</p>
-          <ul>
-            <li>
-              <span className="styled-text">{"Stellar Distance: "}</span>{" "}
-              {stellarDistance + " Parsecs"}
-            </li>
-            <li>
-              <span className="styled-text">{"Effectve Temperature: "}</span>{" "}
-              {effectiveTemperature + " K"}
-            </li>
-            <li>
-              <span className="styled-text">{"Log: "}</span> {log + " cm/s**2"}
-            </li>
-            <li>
-              <span className="styled-text">{"Radius: "}</span>{" "}
-              {radius + " R_Sun"}
-            </li>
-          </ul>
-        </div>
-      </div> */}
       </aside>
     </>
   );
