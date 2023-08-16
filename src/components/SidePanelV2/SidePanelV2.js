9c9b485dd132d7fb6d009bca601211fee871c1f8
Laura Hiller <Hillerelaura@gmail.com>
2023-08-15 17:59:42 -0700

    Added focus to planet carousel and removed old side panel

--- src/components/SidePanelV2/SidePanelV2.js
+++ src/components/SidePanelV2/SidePanelV2.js
@@ -1,12 +1,12 @@
 import "./SidePanelV2.scss";
 import { React, useState } from "react";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md"
-import returnToSpace from '../../assets/emojione-v1_milky-way.svg'
-import planetIcon from '../../assets/HighlightedPlanet.svg'
-import starIcon from '../../assets/Star.svg'
-import planetdata from '../../components/Scene/ExoplanetHelper.js'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import returnToSpace from "../../assets/emojione-v1_milky-way.svg";
+import planetIcon from "../../assets/HighlightedPlanet.svg";
+import starIcon from "../../assets/Star.svg";
+import planetdata from "../../components/Scene/ExoplanetHelper.js";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const defaultValue = {
   name: "",
@@ -27,48 +27,53 @@ function SidePanel({ planet, planetSelected, selectedPlanet }) {
     planet ? planet : defaultValue;
   const { stellarDistance, effectiveTemperature, log, radius } = starData;
 
-  const [hovering, setHovering] = useState(false)
+  const [hovering, setHovering] = useState(false);
 
   const handleReturnClick = () => {
-    window.location.reload(false)
-  }
+    window.location.reload(false);
+  };
 
   const handleHover = () => {
-    setHovering(true)
-  }
+    setHovering(true);
+  };
 
   const handleHoverLeave = () => {
-    setHovering(false)
-  }
+    setHovering(false);
+  };
 
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 2000, min: 992 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 992, min: 768 },
-      items: 4
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 768, min: 576 },
-      items: 3
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 576, min: 0 },
-      items: 1
-    }
-  }
+      items: 1,
+    },
+  };
 
   const CustomButtonGroupAsArrows = ({ next, previous }) => {
-    return (<>
-      <div>
-        <button onClick={previous} className="carousel-arrow left-arrow"><MdKeyboardArrowLeft /></button>
-      </div>
-      <div>
-        <button onClick={next} className="carousel-arrow right-arrow"><MdKeyboardArrowRight /></button>
-      </div>
-    </>
+    return (
+      <>
+        <div>
+          <button onClick={previous} className="carousel-arrow left-arrow">
+            <MdKeyboardArrowLeft />
+          </button>
+        </div>
+        <div>
+          <button onClick={next} className="carousel-arrow right-arrow">
+            <MdKeyboardArrowRight />
+          </button>
+        </div>
+      </>
     );
   };
 
@@ -81,8 +86,17 @@ function SidePanel({ planet, planetSelected, selectedPlanet }) {
           <p className="panel-title"> {name}</p>
 
           <div className="button-wrap">
-            <button className="return" onClick={handleReturnClick} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave}>
-              <img src={returnToSpace} alt=" returnToSpace" className="return-hover" />
+            <button
+              className="return"
+              onClick={handleReturnClick}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHoverLeave}
+            >
+              <img
+                src={returnToSpace}
+                alt=" returnToSpace"
+                className="return-hover"
+              />
             </button>
             {hovering && <p className="hover-text">Return to space </p>}
           </div>
@@ -93,18 +107,24 @@ function SidePanel({ planet, planetSelected, selectedPlanet }) {
             <p>Exoplanet Data</p>
             <ul>
               <li>
-                <span className="styled-text">{"Disposition: "} {disposition}</span>
+                <span className="styled-text">
+                  {"Disposition: "} {disposition}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Right Ascension: "} {rightAscension}</span>
-
+                <span className="styled-text">
+                  {"Right Ascension: "} {rightAscension}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Declination: "} {declination} </span>
-
+                <span className="styled-text">
+                  {"Declination: "} {declination}{" "}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Distance: "} {distance + " Parsecs"}</span>
+                <span className="styled-text">
+                  {"Distance: "} {distance + " Parsecs"}
+                </span>
               </li>
             </ul>
           </div>
@@ -113,16 +133,27 @@ function SidePanel({ planet, planetSelected, selectedPlanet }) {
             <p>Stellar Data</p>
             <ul>
               <li>
-                <span className="styled-text">{"Stellar Distance: "} {stellarDistance + " Parsecs"}</span>
+                <span className="styled-text">
+                  {"Stellar Distance: "} {stellarDistance + " Parsecs"}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Effectve Temperature: "}{effectiveTemperature + " K"}</span>
+                <span className="styled-text">
+                  {"Effectve Temperature: "}
+                  {effectiveTemperature + " K"}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Log: "}{log + " cm/s**2"}</span>
+                <span className="styled-text">
+                  {"Log: "}
+                  {log + " cm/s**2"}
+                </span>
               </li>
               <li>
-                <span className="styled-text">{"Radius: "}{radius + " R_Sun"}</span>
+                <span className="styled-text">
+                  {"Radius: "}
+                  {radius + " R_Sun"}
+                </span>
               </li>
             </ul>
           </div>
@@ -139,24 +170,26 @@ function SidePanel({ planet, planetSelected, selectedPlanet }) {
               customButtonGroup={<CustomButtonGroupAsArrows />}
               renderButtonGroupOutside={true}
             >
-              {planetdata.map((planetItem) => (
+              {planetdata.map((planetItem, index) => (
                 <div
                   key={planetItem.name}
                   class="planet-item"
-                  onClick={() => planetSelected(planetItem)}>
+                  onClick={() => planetSelected(planetItem)}
+                >
                   <p className="planet-item-name">{planetItem.name}</p>
                   <img
-                    src={starIcon}
-                    alt={`Image of ${planetItem.name}`}
-                    style={{ width: '3rem' }}
+                    src={planetItem.photo}
+                    alt={`${planetItem.name}`}
+                    style={{ width: "3rem", borderRadius: "100%" }}
                   />
+                  {/* <div className="planet-sphere"></div> */}
                 </div>
               ))}
             </Carousel>
           </section>
         </section>
-        <p id="selected-planet-name">Exoplanet {name}</p>
-      </aside >
+        <p className="selected-planet-name">Exoplanet {name}</p>
+      </aside>
     </>
   );
 }
--- src/pages/Visualize.js
+++ src/pages/Visualize.js
@@ -1,6 +1,5 @@
 import Scene from "../components/Scene/Scene";
-import SidePanel from "../components/SidePanel/SidePanel";
-import SidePanelV2 from "../components/SidePanelV2/SidePanelV2"
+import SidePanelV2 from "../components/SidePanelV2/SidePanelV2";
 import { React, useState, useEffect, useRef } from "react";
 import planetdata from "../components/Scene/ExoplanetHelper";
 import { useLoader } from "@react-three/fiber";
@@ -11,15 +10,13 @@ import TanGiantImg from "../assets/textures/tanGasGiant.png";
 
 import "./Visualize.scss";
 
-
-
 function Visualize() {
   const [selectedPlanet, setSelectedPlanet] = useState();
   const planetSelected = (planet) => {
     // if planet
     // record camera position in some variable, galacticCameraPosition
     // Set star position based on planet.position.x - orbital radius
-      // may need another variable/piece of state for System -> Sun.position values & might want to look into random application of different color star textures (or just randomly applying a red or blue default color, if it changes the existing sun texture)
+    // may need another variable/piece of state for System -> Sun.position values & might want to look into random application of different color star textures (or just randomly applying a red or blue default color, if it changes the existing sun texture)
     // lerp camera.position to x midpoint between planet and star, neutral y position, z position 1.25x midpoint distance (?)
     // camera.lookAt x midpoint
     // else if !selectedPlanet && galacticCameraPosition
@@ -31,34 +28,43 @@ function Visualize() {
   // look into Suspense built-in component for async delay on texture load
 
   //Textures downloaded under CC0 from OpenGameArt.org
-  const textureOptions = useLoader(TextureLoader, [BlueGiantImg, RockyWorldImg, TanGiantImg])
+  const textureOptions = useLoader(TextureLoader, [
+    BlueGiantImg,
+    RockyWorldImg,
+    TanGiantImg,
+  ]);
 
-  const textureSelection = Array.from(Array(planetdata.length), ()=> { return Math.floor(Math.random() * textureOptions.length)})
+  const textureSelection = Array.from(Array(planetdata.length), () => {
+    return Math.floor(Math.random() * textureOptions.length);
+  });
 
   const applyTextures = (planetData) => {
     planetData.forEach((planet, i) => {
-      planetdata[i] = {...planet, texture: textureOptions[textureSelection[i]]}
-    })
-  }
+      planetdata[i] = {
+        ...planet,
+        texture: textureOptions[textureSelection[i]],
+      };
+    });
+  };
 
   //Modify data here to include textures before passing down
-  useEffect(()=>{
-    applyTextures(planetdata)
-  }, [])
+  useEffect(() => {
+    applyTextures(planetdata);
+  }, []);
 
   return (
     <div>
-      <Scene 
-        planetSelected={planetSelected} 
-        selectedPlanet={selectedPlanet} 
-        planetdata={planetdata} />
+      <Scene
+        planetSelected={planetSelected}
+        selectedPlanet={selectedPlanet}
+        planetdata={planetdata}
+      />
 
-      {/* <SidePanel planet={selectedPlanet} /> */}
-
-      <SidePanelV2 
+      <SidePanelV2
         planet={selectedPlanet}
-        planetSelected={planetSelected} 
-        selectedPlanet={selectedPlanet}  />
+        planetSelected={planetSelected}
+        selectedPlanet={selectedPlanet}
+      />
     </div>
   );
 }
