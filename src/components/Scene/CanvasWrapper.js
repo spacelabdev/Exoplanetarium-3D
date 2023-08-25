08b94abb7c5178fa90ed50519403746e77d9b06b
jrkozak92 <jrkozak92@gmail.com>
2023-08-24 21:43:43 -0500

    Working version

--- src/components/Scene/CanvasWrapper.js
+++ src/components/Scene/CanvasWrapper.js
@@ -4,7 +4,7 @@ import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import * as THREE from 'three'
 import Scene from './Scene';
 
-const CanvasWrapper = ({selectedPlanet, planetSelected, planetdata}) => {
+const CanvasWrapper = ({settings, selectedPlanet, planetSelected, planetdata}) => {
 
     return (
         <div id="canvas-wrap">
